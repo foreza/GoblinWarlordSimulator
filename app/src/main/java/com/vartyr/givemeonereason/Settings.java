392c26964aa263358be743fce81844228ef62e08
Jason Chiu <jason.chiu@inmobi.com>
2018-10-31 16:36:18 -0700

    Fix layout related issues

--- app/src/main/java/com/vartyr/givemeonereason/Settings.java
+++ app/src/main/java/com/vartyr/givemeonereason/Settings.java
@@ -3,16 +3,11 @@ package com.vartyr.givemeonereason;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ListOfIDGAF extends AppCompatActivity {
-
-
-    String [] listOfIDGAF = {""};       // List of things we actually don't give a flying f about.
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_of_idgaf);
+        setContentView(R.layout.activity_settings);
     }
-
-
 }
